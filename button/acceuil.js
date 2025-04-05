1dfdb750facd5b95682084396449e00ea00356a2
vava408 <valentinlemaitre06@gmail.com>
2025-04-05 17:28:00 +0200

    Mise a jour de l accueil Creation boutton help

--- button/acceuil.js
+++ button/acceuil.js
@@ -1,7 +1,24 @@
+const Discord = require("discord.js");
+
 module.exports = {
 	name: "accueil",
 	async run(client, interaction) {
-		console.log(`${client.user.tag} a cliqué sur le bouton accueil !`);
-		await interaction.update({ content: 'Vous avez cliqué sur le bouton accueil !', components: [] });
+
+		const embed = new Discord.EmbedBuilder()
+			.setTitle("Menu Principal")
+			.setDescription("Bienvenue dans le menue prinncipal de Airbot ! C est ici que tout les commandes et leur explication se trouve ! \n\nVous trouver aussi le liens vers le dashboard https://www.airbot.adkynet.eu/dashboard/dahboard.php acecible apres vousetes créer un compte et l avoir lier a discord ddans profils!")
+			.setAuthor({ name: interaction.client.user.tag })
+			.setTimestamp();
+
+		const accueil = new Discord.ButtonBuilder()
+			.setCustomId('accueil')
+			.setLabel('Accueil')
+			.setEmoji('🏠')
+			.setStyle(Discord.ButtonStyle.Danger); // Définir le style du bouton
+
+		const row = new Discord.ActionRowBuilder()
+			.addComponents(accueil);
+
+		await interaction.update({embeds: [embed], components: [row],});
 	},
 };
--- commands/utils/help.js
+++ commands/utils/help.js
@@ -5,19 +5,28 @@ module.exports = {
     description: 'Affiche les commandes et les explique',
     execute(message, args) {
 		const embed = new Discord.EmbedBuilder()
-            .setTitle("Menu Principal")
-            .setDescription("Voici la liste des commandes disponibles :\n\n**!ping** - Vérifie la latence du bot\n**!help** - Affiche ce message")
-            .setAuthor({ name: message.client.user.tag })
-            .setTimestamp();
+			.setTitle("Menu Principal")
+			.setDescription("Bienvenue dans le menue prinncipal de Airbot ! C est ici que tout les commandes et leur explication se trouve ! \n\nVous trouver aussi le liens vers le dashboard https://www.airbot.adkynet.eu/dashboard/dahboard.php acecible apres vousetes créer un compte et l avoir lier a discord ddans profils!")
+			.setAuthor({ name: message.client.user.tag })
+			.setTimestamp();
 
 		const accueil = new Discord.ButtonBuilder()
 			.setCustomId('accueil')
-            .setLabel('Accueil')
+			.setLabel('Accueil')
 			.setEmoji('🏠')
+			.setStyle(Discord.ButtonStyle.Success); // Définir le style du bouton
+
+		const moderation = new Discord.ButtonBuilder()
+			.setCustomId('moderation')
+			.setLabel('moderation')
+			.setEmoji('⛔')
 			.setStyle(Discord.ButtonStyle.Danger); // Définir le style du bouton
 
+
 		const row = new Discord.ActionRowBuilder()
-			.addComponents(accueil);
+			.addComponents(accueil)
+			.addComponents(moderation);
+
 
         message.channel.send({
             embeds: [embed],
