ece495d054c56e41bb49e7deff361516f4fe6a8b
vava408 <valentinlemaitre06@gmail.com>
2025-04-26 18:38:23 +0200

    ajout de la commande infoserveur

---
+++ commands/information/infouser.js
@@ -0,0 +1,35 @@
+
+const db = require('../../connexion.js'); 
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'infoserv', 
+	description: 'Joue à la machine à sous ! ', 
+	async execute(message, args) {
+
+		const username = message.mentions.members.first() || message.member;
+		const user = message.mentions.users.first() || message.author
+		// Obtenir tous les rôles du membre
+		const roles = username.roles.cache.map(role => role.name);
+
+
+
+
+
+
+		const embed = new Discord.EmbedBuilder()
+			.setColor("#0004E1")
+			.setTitle('Informations sur ' + user.tag)
+			.setImage(user.displayAvatarURL({ dynamic: true, size: 512 }))
+			.addFields(
+				{ name: 'Pseudo', value: `${user.username}`, inline: true },
+				{ name: 'ID', value: `${user.id}`, inline: true },
+				{ name: 'Bot', value: `${user.bot ? '🟢' : '🔴'}`, inline: true },
+				{ name: 'Date de création du compte', value: `<t:${parseInt(user.createdTimestamp / 1000)}:f> (<t:${parseInt(user.createdTimestamp / 1000)}:R>)` },
+				{ name: 'Roles du membres', value: `${roles.join(', ')}` }
+			)
+
+
+		message.channel.send({ embeds: [embed] });
+	}
+};
