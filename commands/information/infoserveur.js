9ddcd29ee1994e65de3422a70cc107bf88522e6e
vava408 <valentinlemaitre06@gmail.com>
2025-04-26 18:37:50 +0200

    ajout de la commande infoserveur

---
+++ commands/information/infoserveur.js
@@ -0,0 +1,27 @@
+const db = require('../../connexion.js'); 
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'infoserv', 
+	description: 'Joue à la machine à sous ! ', 
+	async execute(message, args) {
+
+		const embed = new Discord.EmbedBuilder()
+			.setColor("#ff0000")
+			.setTitle('Informations sur le serveur ' + message.guild.name)
+			.setImage(message.guild.iconURL())
+			.addFields(
+				{ name: 'Nom du serveur', value: `${message.guild.name}`, inline: true },
+				{ name: 'ID du serveur', value: `${message.guild.id}`, inline: true },
+				{ name: 'Proprietaire du serveur ', value: ` <@${message.guild.ownerId}>`, inline: true },
+				{ name: 'Membres', value: `${message.guild.memberCount}`, inline: true },
+				{ name: 'Nombre de role', value: `${message.guild.roles.cache.size}`, inline: true },
+				{ name: 'Régions', value: `${message.guild.region}`, inline: true },
+				{ name: 'Niveau de securiter', value: `${message.guild.verificationLevel}`, inline: true },
+				{ name: 'Date de création du serveur', value: `${message.guild.createdAt.toDateString()}` },
+			)
+
+
+		message.channel.send({ embeds: [embed] });
+	}
+};
